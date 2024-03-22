c7dec9fd87c8f18ecdbc0932fc253a215a0d29d2
Sergei <sergei.smirnov001@ivkhk.ee>
2024-03-22 09:17:37 +0200

    startAdmin.php admin start pages writing

--- admin/viewAdmin/startAdmin.php
+++ admin/viewAdmin/startAdmin.php
@@ -1,1 +1,13 @@
-<?php
+<?php ob_start() ?>
+<article>
+    <div id="manin" class="container">
+        <h3>Админ панель</h3>
+        <div class="row">
+            <p>Админ панель</p>
+        </div>
+    </div>
+</article>
+
+<?php $content = ob_get_clean();?>
+
+<?php include "viewAdmin/template/layout.php";

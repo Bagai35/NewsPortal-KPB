af31066e2e6e60da7d70728961228dddbe565d7d
Sergei <sergei.smirnov001@ivkhk.ee>
2024-03-22 10:26:53 +0200

    NPKBP-37 прописал контроллер для админа

--- admin/controllerAdmin/controllerAdmin.php
+++ admin/controllerAdmin/controllerAdmin.php
@@ -1,1 +1,23 @@
 <?php
+class controllerAdmin {
+    public static function formLoginSite() {
+        include_once ('viewAdmin/formLogin.php');
+    }
+    public static function loginAction() {
+        $logIn = modelAdmin::userAuthentication();
+        if (isset($logIn) and $logIn==true) {
+            include_once ('viewAdmin/startAdmin.php');
+        }
+        else {
+            $_SESSION['errorString'] = 'Неправильное имя ользователя или пароль';
+        }
+    }
+    public static function logoutAction() {
+        modelAdmin::userLogout();
+        include_once ('viewAdmin/formLogin.php');
+    }
+    public static function error404() {
+        include_once ('viewAdmin/error404.php');
+    }
+}
+?>
